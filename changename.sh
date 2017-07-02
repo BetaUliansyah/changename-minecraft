afee112197848daad00f6b47c4e288f2d805c414
Beta Uliansyah <beta.uliansyah@gmail.com>
2017-07-02 16:17:24 +0700

    Create changename.sh

---
+++ changename.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ $# -eq 0 ]; then
+    echo "Supply new displayName as argument"
+    exit 1
+fi
+
+PROFILE=~/.minecraft/launcher_profiles.json
+OLDNAME=`grep displayName $PROFILE | cut -d'"' -f4`
+echo -e "Old name: $OLDNAME"
+sed -i -- "s/$OLDNAME/$1/g" $PROFILE
+echo -e "New name: $1"
+echo -e "Read file $PROFILE: `grep displayName $PROFILE | cut -d'"' -f4`"
+chmod 400 $PROFILE
